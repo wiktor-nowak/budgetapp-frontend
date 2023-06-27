0ec9c9ebfadf156ac4bf03aeaf94c888ff1b8822
wiktor-nowak <nowak.wiktor95@gmail.com>
2023-06-27 10:43:24 +0200

    login page

--- src/features/api/userApi.js
+++ src/features/api/userApi.js
@@ -1,30 +1,40 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userApi = createApi({
-    reducerPath:"userApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl:"http://localhost:5000/",
-        prepareHeaders: async (headers, {getState}) => {
-            const token = await getState().auth.token;
-            if(token) {
-                headers.set("token", token);
-            };
-            return headers;
-        }
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/",
+    prepareHeaders: async (headers, { getState }) => {
+      const token = await getState().auth.token;
+      if (token) {
+        headers.set("token", token);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUser: builder.query({
+      query: () => "dashboard",
     }),
-    endpoints: (builder) => ({
-        getUser: builder.query({
-            query: () => 'dashboard'
-        }),
-        registerUser: builder.mutation({
-            query: (data) => ({
-                url:"auth/register",
-                method:"POST",
-                body:data
-            })
-        })
-    })
-})
+    registerUser: builder.mutation({
+      query: (data) => ({
+        url: "auth/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (data) => ({
+        url: "auth/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
 
-export const {useGetUserQuery, useRegisterUserMutation} = userApi;
+export const {
+  useGetUserQuery,
+  useRegisterUserMutation,
+  useLoginUserMutation,
+} = userApi;
